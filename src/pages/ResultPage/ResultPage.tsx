ce87067a4ccd3b61eb939301f70a54df8768c3ab
gyuminJJANG <ggavwu789@gmail.com>
2026-07-10 04:23:38 +0900

    feat: 결과 페이지 구현 및 쿼리 파라미터 연동

--- src/pages/ResultPage/ResultPage.tsx
+++ src/pages/ResultPage/ResultPage.tsx
@@ -1,1 +1,22 @@
-export default function ResultPage(){return <div>결과 화면</div>;}
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+export default function ResultPage(){
+    const navigate=useNavigate();
+    const [searchParams]=useSearchParams();
+
+    const genre =searchParams.get("genre");
+    const era=searchParams.get("era");
+
+    return (
+        <main>
+            <h2>FlixDrop 드롭 결과</h2>
+            <div>
+                <h3>주소창에서 읽어온 데이터</h3>
+                <p>드롭된 장르 코드: {genre}</p>
+                <p>드롭된 시대 코드: {era}</p>
+            </div>
+
+            <button onClick={()=>navigate("/")}>필터 다시 고르기</button>
+        </main>
+    );
+}
