af1c0a9218af10829e59ebb87a72f82b94ec0781
rahwan10 <donggeon10@naver.com>
2026-07-09 22:02:26 +0900

    feat:각 기능별 화면 뼈대 잡기

---
+++ src/screen/List/ListScreen.tsx
@@ -0,0 +1,8 @@
+export function ListScreen() {
+  return (
+    <div>
+      <h1>List Screen</h1>
+      <p>This is the list screen of the app.</p>
+    </div>
+  );
+}
---
+++ src/screen/Search/SearchScreen.tsx
@@ -0,0 +1,8 @@
+export function SearchScreen() {
+  return (
+    <div>
+      <h1>Search Screen</h1>
+      <p>This is the search screen of the app.</p>
+    </div>
+  );
+}
