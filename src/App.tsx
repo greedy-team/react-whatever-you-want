ab0256c72b1755b1ed4c93f99a86f4819438996e
gyuminJJANG <ggavwu789@gmail.com>
2026-07-30 21:34:14 +0900

    fix: 404 에러 시 Layout 내부에서 ErrorPage가 렌더링되도록 path '*' 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,10 @@ const router = createBrowserRouter([
             path: "history",
             element: <HistoryPage />,
           },
+          {
+            path: "*",
+            element: <ErrorPage />,
+          },
         ],
       },
     ],
